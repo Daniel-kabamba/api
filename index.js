c29e758ee5ed1976f8d4323d6fb7967fdef311fa
Daniel <mandanico16@gmail.com>
2025-12-02 13:08:15 +0200

    execution

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/ap', (req, res) => {
   res.send('Hello, World!');
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// GET;PUT;DELETE;PATCH;POST;OPTIONS;HEAD
